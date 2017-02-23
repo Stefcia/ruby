072a4af798433985f9772a90a190270de319d8a5
Stefcia <nowar3@wp.pl>
2017-02-23 19:08:41 +0100

    Robot.rb

--- LeR/Robot.rb
+++ LeR/Robot.rb
@@ -63,6 +63,18 @@ class Robot
 	def dodatki
 		return @dodatki
 	end
+	def zdolność_obrony
+		zdolność_obrony
+	end
+	def zdolność_obrony= (nowa_zdolność)
+		@zdolność_obrony = nowa_zdolność
+	end
+	def zdolność_ataku= (zdolność_a)
+		@zdolność_ataku = zdolność_a
+	end
+	def zdolność_ataku
+		zdolność_ataku
+	end
 	#zapisywanie Robocików do JSONa - nie ruszać! #http://stackoverflow.com/questions/3226054/how-to-convert-a-ruby-object-to-jsona
 	def as_json(options={})
         {
@@ -101,9 +113,11 @@ class Robot
 	end
 end
 class RoboPtak < Robot # przychodzi w poziomie II
-	def initialize(skrzydła, nogi, nazwa)
+	def initialize(skrzydła, nogi, nazwa,zdolność_ataku=10,zdolność_obrony=67)
 		@dodatki = []
 		@sila = 901000
+		@zdolność_obrony = zdolność_obrony
+		@zdolność_ataku = zdolność_ataku
 		@skrzydła = skrzydła
 		@nogi = nogi
 		@nazwa = nazwa
@@ -115,13 +129,15 @@ class RoboPtak < Robot # przychodzi w poziomie II
 		niestety, stracił siłę o #{silaw*2}.
 		teraz ma #{@sila}."
 	end
-	def as_json(options={})
+	def as_json(*options={})
 		{
-			sila: @sila,
+			siła: @sila,
             zwinnosc: @zwinnosc,
             dodatki: @dodatki,
+            zdolność_ataku: @zdolność_ataku,
 			nazwa: @nazwa,
-			sila: @@sila,
+			współna_siła: @@sila,
+			zdolność_obrony: @zdolność_obrony,
 			skrzydła: @skrzydła,
 			nogi: @nogi
 		}
@@ -135,13 +151,15 @@ class Wynalazca < Robot # przychodzi w poziomie V
 			mózg_dla_robota: 6790,
 			drewno: 8980
 		}
-	def initialize(iśrubokrętów=45, igwoździ=56, imłotków=45, iśrubek=56)
-		@nazwa = ["Lucas3","Eltra#{[67,1,2,142,2,34,67,475,76].sample}","R#{[5,6,7,8,9,10,11,12,13,14,151,16,15,17,nil,18,19,20].sample}","Lucas#{[90,900,1,2,3,4,5,7,6,7,8,nil].sample}","Lucas67","Eltra 34","Pierd1","R1","R2","R3",'R4',"Pierd#{1,2,3,4,5,6,7,8,9,10,45,1797,nil}"].sample
+	def initialize(zdolność_ataku=56,iśrubokrętów=45, igwoździ=56, imłotków=45, iśrubek=56, zdolność_obrony=89)
+		@nazwa = ["Lucas3","Ullo60","Eltra#{[67,1,2,142,2,34,67,475,76].sample}","R#{[5,6,7,8,9,10,11,12,13,14,151,16,15,17,nil,18,19,20].sample}","Robo#{[34,23,456,345,645,465]}","#{["Los#{[7,89,68,567,56].sample}","R56","Niezwykły Wynalazca#{[3,4,5,6,71,2,3,1,4,5,6,7,8,9].sample}"].sample}","Lucas#{[90,900,1,2,3,4,5,7,6,7,8,nil].sample}","Lucas67","Eltra 34","Pierd1","R1","R2","R3",'R4',"Pierd#{1,2,3,4,5,6,7,8,9,10,45,1797,nil}","Ullo70","Ullo#{[30,300,3,12,78,74,85,24,56,34,5].sample}"].sample
 		@iśrubek = iśrubek # ilość śrubek
 		@imłotków = imłotków # ilość młotków
 		@igwoździ = igwoździ # ilość gwóździ
+		@zdolność_obrony = zdolność_obrony
 		@iśrubokrętów = iśrubokrętów # ilość śrubokrętów
 		@zwinnosc = [1,2,89,45,768,8990,565656,12, 89, 74555, 562].sample
+		@zdolność_ataku = zdolność_ataku
 		@sila = [nil, 0,34,67,nil,4,57,78, 10008320009900].sample
 		@dodatki = {}
 		@wynalazkir = []
@@ -155,15 +173,17 @@ class Wynalazca < Robot # przychodzi w poziomie V
 		end
 		@dodatki.merge (dodatk)
 	end
-	def as_json(options={})
+	def as_json(*options={})
 		{
 			nazwa: @nazwa,
 			dodatki: @dodatki,
 			zwinnosc: @zwinnosc,
-			siłaa: @sila,
+			siła: @sila,
+			zdolność_obrony: @zdolność_obrony,
 			ilość_śrubokrętów: @iśrubokrętów,
 			ilość_młotków: @imłotków,
 			ilość_gwoździ: @igwoździ,
+			zdolność_ataku: @zdolność_ataku,
 			ilość_śrub: @iśrubek,
 			wspólne_dodatki: @@dodatki,
 			wynalazki: @wynalazkir,
@@ -198,7 +218,7 @@ class Wynalazca < Robot # przychodzi w poziomie V
 			ilość jeszcze wspólnych wynalazków: #{@@wynalazki}"
 			end
 		end
-		def as_json(options={})
+		def as_json(*options={})
 			{
 			dodatki: @dodatki,
 			zwinnosc: @zwinnosc,
@@ -230,11 +250,13 @@ class Lekarz < Robot # przychodzi w poziomi X.
 		robo_strzykawka: 78,
 		"robo-słuchawka do słuchania pulsowania śrubek" => 6909
 	}
-	def initialize(mądrość=[9,12,23,34,100,20,19,18,17,16,1000,167].sample)
+	def initialize(zdolność_obrony=10000,zdolność_ataku=10,mądrość=[9,12,23,34,100,20,19,18,17,16,1000,167].sample)
 		@mądrość = mądrość
+		@zdolność_ataku = zdolność_ataku
 		@zwinnosc = [45,34,45,163,66].sample
 		@siła = [12,35,636,7685,454512].sample
 		@dodatki = []
+		@zdolność_obrony = zdolność_obrony
 		@nazwa = ["Lucas50","Ullo60","Olli10","Robo40","Linno5","Etto30","Tikko20","Osa100"].sample
 	end
 	def self.co?
@@ -242,11 +264,21 @@ class Lekarz < Robot # przychodzi w poziomi X.
 		i ma dodatki: #{@@dodatki}"
 	end
 	def self.dd (new_dd={})
-		new_dd.each do |klucz, no_klucz|
-			print "czy chcesz mieć w #{@@dodatki}: {#{klucz}: #{no_klucz}}?"
-			tt = gets.chomp
-			if tt == 'tak'
-				@@nieosobiste_dodatki = @@nieosbiste_dodatki.merge({"#{klucz}" => no_klucz})
+		if new_dd.class == Hash
+			new_dd.each do |klucz, no_klucz|
+				print "czy chcesz mieć w #{@@dodatki}: {#{klucz}: #{no_klucz}}?"
+				tt = gets.chomp
+				if tt == 'tak'
+					@@nieosobiste_dodatki = @@nieosbiste_dodatki.merge({"#{klucz}" => no_klucz})
+				end
+			end
+		else
+			new_dd.each do |klucz|
+				print "czy chcesz mieć w #{@@dodatki}: {#{klucz}: #{no_klucz}}?"
+				tt = gets.chomp
+				if tt == 'tak'
+					@@nieosobiste_dodatki = @@nieosbiste_dodatki.merge({"#{klucz}" => no_klucz})
+				end
 			end
 		end
 	end
@@ -256,15 +288,17 @@ class Lekarz < Robot # przychodzi w poziomi X.
 	def self.punkty
 		return @@punkty
 	end
-	def as_json(options={})
+	def as_json(*options={})
 		{
 			nazwa: @nazwa,
 			siła: @siła,
+			zdolność_ataku: @zdolność_ataku
 			zwinność: @zwinnosc,
 			dodatki: @dodatki,
 			mądrość: @mądrość,
 			ilość_wspólnych_dodatków: @@nieosobiste_dodatki,
-			wspólne_punkty: @@punkty
+			wspólne_punkty: @@punkty,
+			zdolność_obrony: @zdolność_obrony
 		}
 	end
 end
@@ -273,6 +307,23 @@ r2 = RoboPtak.new(70,65,"Ullo70")
 r3 = Wynalazca.new(677,50,455,677)
 r4 = Lekarz.new(666)
 domyślne_roboty = ["#{r1}","#{r2}","#{r3}","#{r4}"]
-def zapisz_roboty_w_(gdzie, roboty)
-	roboty
+def zapisz_roboty_w_(gdzie, *roboty)
+	*roboty.each do |Robocik|
+		print "czy nasz robot '#{Robocik}' ma być w pliku #{gdzie}(wpisz tak, jeżeli tak)?"
+		tlubn = gets.chomp
+		if tlubn == 'tak'
+			puts "nasz robot '#{Robocik}' zostaje zapisywany..."
+			file = File.open(gdzie, 'w')
+			puts "dobra. otworzyliśmy plik."
+			file.write("#{Robocik}")
+			puts "OK.Robot został zapisywany."
+			file.close
+			puts "już gotowe."
+		end
+	end
+end
+def wyświetl_coś_w_(gdzie)
+	file = File.open(gdzie, 'r')
+	return file.read
+	file.close
 end
