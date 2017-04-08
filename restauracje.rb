15e7a90dd990b91422d01dde374e7cbe5f4fba6f
Stefcia <nowar3@wp.pl>
2017-04-08 12:22:48 -0400

    nowy_bar+restauracja.rb

--- restauracje.rb
+++ restauracje.rb
@@ -51,11 +51,12 @@ class Restauracja
 		puts "#{@@Wszystkie_stoły}"
 	end
 	def zjedzmy(proponuję)
+		@@Wszystkie_stoły = [@@stół_Wegetarian, @@stół_Roślinożerców, @@stół_PCz, @@stół_Wegan, @@stół_mięsożerców, @@stół_wszystkożerców]
 		puts "dla stołu #{@@stół_Roślinożerców} mam trawę+#{['herbatę Roibos', 'oranżadę z jagód', 'lemoniadę z truskawek', 'sok z borówek'].sample}.
 		do stołu #{@@stół_PCz} mam #{["#{proponuję}",'kurczak i surówka+herbata Matcha gratis!', 'purre z ziemniaków i marchewki z dorszem i kawą.'].sample}
 		dla stołu #{@@stół_mięsożerców} mam #{[:sarnę, :borsuka, :niedźwiedzia].sample}!
-		do stołu #{@@stół_Wegan} mam #{["#{proponuję}", "#{proponuję} i lemoniadę", "#{proponuję}].sample} i #{["napój z róży", "jogurt z mleka migdałowego"].sample}"]}!
-		dla stołu #{@@stół_wszystkożerców} mam #{["'pyszną' padlinkę łosia","", "trawę+padlinę+herbatkę malinową"].sample} i pyszny dodatek: #{[:bezkręgowiec, "Ślimak", "gąbka prosto z morza!!"].sample}!
+		do stołu #{@@stół_Wegan} mam #{["#{proponuję}", "#{proponuję} i lemoniadę", "budyń z kaszy jaglanej", "#{proponuję}, soczewica i Yerbata (lub herbata z trawą cytrynową :) )"].sample} i #{["napój z róży", "jogurt z mleka migdałowego"].sample}]}!
+		dla stołu #{@@stół_wszystkożerców} mam #{["'pyszną' padlinkę łosia","#{proponuję}, wędliny i trawa cytrynowa+herbata Yerba" , "trawę+padlinę+herbatkę malinową"].sample} i pyszny dodatek: #{[:bezkręgowiec, "Ślimak", "gąbka prosto z morza!!"].sample}!
 		do stołu #{@@Wszystkie_stoły} mam #{proponuję}.
 		dla stołu #{@@stół_Wegetarian} mam #{["#{proponuję}", "#{proponuję}, ryż z cukinią i sałatkę owocową"].sample}+#{["sok z truskawki.", :sok, "kawa!", "Matcha!"].sample}"
 	end
@@ -104,7 +105,7 @@ class Restauracja
 				@@stół_Roślinożerców = @@stół_Roślinożerców.merge({"#{imię}" => Time.now - (rand(2000) + 101)})
 				break
 			end
-			puts "czy #{imię} jast człowiekiem i nie je: mięsa, jogurtów, mleka, jaj i innych produktów odzwierzęcych? "
+			print "czy #{imię} jast człowiekiem i nie je: mięsa, jogurtów, mleka, jaj i innych produktów odzwierzęcych? "
 			t_lub_nie = gets.chomp
 			if t_lub_nie == 'tak'
 				Wegan::NIE_JEM.each do |nie_jem|
@@ -113,6 +114,58 @@ class Restauracja
 				@@stół_Wegan = @@stół_Wegan.merge({"#{imię}" => Time.now - -23577870099})
 				break
 			end
+			print "czy #{imię} je tylko mięso???"
+			taklubn = gets.chomp
+			if taklubn == 'tak.'
+				Mięsożerca::JEM.each do |jem|
+					puts "jem #{jem}!"
+				end
+				@@stół_wszystkożerców = @@stół_wszystkożerców.merge({"#{imię}" => Time.now + rand(880)+1})
+			end
+		end
+	end
+	def regulamin
+		puts "I. Nie wolno palić papierosów.
+		II. #{@nazwa_restauracji} jest miejscem publicznym.
+		III. W #{@nazwa_restauracji} obowiązują WSZYSTKIE ZASADY miejsc PUBLICZNYCH.
+		UWAGA! W terenie restauracjii właściciel restauracji, #{@imię}, ma prawo* wręczyć mandat lub zakaz wchodzenia na teren restauracjii(co też każe mandatem).
+
+
+		* - #{@imię} ma PRAWO wręczyć MANDAT TYLKO WTEDY, gdy KTOŚ ZACHOWUJE się NIEKULTURALNI LUB nie PRZESTRZEGA ZASAD REGULAMINU(^)."
+	end
+	def self.odświerz
+		@@Wszystkie_stoły = [@@stół_Wegetarian, @@stół_Roślinożerców, @@stół_PCz, @@stół_Wegan, @@stół_mięsożerców, @@stół_wszystkożerców]
+	end
+end
+class Bar < Restauracja
+	@@stół_wszystkożerców = {"dzik" => Time.now + 1}
+	@@stół_PCz = {:filozof => Time.now - (1000 * (199 * (1000 * 100)))}
+	@@stół_Wegetarian = {:ja => Time.now}
+	@@stół_Wegan = {:filozof2 => Time.now - (200 * (1000 * (100 * 1000)))}
+	@@stół_Roślinożerców = {"Bambi" => Time.now + rand(30)}
+	@@stół_mięsożerców = {"Simba" => Time.now + 6}
+	@@Wszystkie_stoły = [@@stół_Wegetarian, @@stół_PCz, @@stół_mięsożerców, @@stół_Roślinożerców, @@stół_Wegan, @@stół_wszystkożerców]
+	def initialize(nazwa_baru="pod siwym rakiem",imię="Sztefi N.",typ_jedzenia=[:Wegetarianin, :Wegan, :PopularnyCzłowiek].sample)
+		puts "cześć! możemy, dodać trochę osób do tych pustych stołow:
+		{}, {}, {}, {}, {}, {}, {}, lub od razu smakować najlepsze dania baru #{nazwa_baru}."
+		@imię = imię
+		@nazwa_baru = nazwa_baru
+		case typ_jedzenia
+		when :Wegan
+			sleep 1
+			puts "Świetnie! znam Twój tryb jedzenia!"
+			@@stół_Wegan = @@stół_Wegan.merge ({"#{imię}" => Time.now})
+		when :Wegetarianin
+			sleep 1
+			puts "Super! Ja też Jestem Wegetarianinem."
+			@@stół_Wegetarian = @@stół_Wegetarian.merge ({"#{imię}" => Time.now})
+		when :PopularnyCzłowiek
+			sleep 1
+			puts "'ludziu popularny'!"
+			@@stół_PCz = @@stół_PCz.merge ({"#{imię}" => Time.now})
+		else
+			puts "#{typ_jedzenia}? taki nie znam."
+			@@Wszystkie_stoły = @@Wszystkie_stoły.push
 		end
 	end
 end
