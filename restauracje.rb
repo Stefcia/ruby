37a900d57f4859d86166319b0be45be8336e837d
Stefcia <nowar3@wp.pl>
2017-04-08 12:01:15 +0200

    Update restauracje.rb
    zmiany w programie!(Mam jeszcze błędy :) )

--- restauracje.rb
+++ restauracje.rb
@@ -8,7 +8,9 @@ module Roślinożerca
 	jem = ["jem prawie to co Wszystkożerca.", 'jem roślimy.', 'jestem pożywieniem mięsożerców.']
 end
 module Wszystkożerca
-	nie_jem = ["Wszystko jem!"]
+	def nie_jem
+		nie_jem = ["Wszystko jem!"]
+	end
 end
 module Mięsożerca
 	jem = ["Jem tylko mięso."]
@@ -16,18 +18,19 @@ end
 module PopularnyCzłowiek
 	nie_jem = [:prawie_nic]
 end
-class Zjedzmy
-	@@stół_wszystkożerców = {}
-	@@stół_PCz = {}
+class Restauracja
+	@@stół_wszystkożerców = {"dzik" => Time.now}
+	@@stół_PCz = {:filozof => Time.now - (1000 * (199 * (1000 * 100)))}
 	@@stół_Wegetarian = {:ja => Time.now}
 	@@stół_Wegan = {}
-	@@stół_Roślinożerców = {"Bambi" => Time.now - rand(3000)}
+	@@stół_Roślinożerców = {"Bambi" => Time.now + rand(30)}
 	@@stół_mięsożerców = {"Simba" => Time.now + 6}
 	@@Wszystkie_stoły = [@@stół_Wegetarian, @@stół_PCz, @@stół_mięsożerców, @@stół_Roślinożerców, @@stół_Wegan, @@stół_wszystkożerców]
-	def initialize(imię="Sztefi N.",typ_jedzenia=[:Wegetarianin, :Wegan, :PopularnyCzłowiek].sample)
-		puts "cześć! możemy, dodać trochę osób do list:
-		{}, {}, {}, {}, {}, {}, {}, lub od razu 'jeść'."
+	def initialize(nazwa_restauracji="pod starym rakiem",imię="Sztefi N.",typ_jedzenia=[:Wegetarianin, :Wegan, :PopularnyCzłowiek].sample)
+		puts "cześć! możemy, dodać trochę osób do tych pustych stołow:
+		{}, {}, {}, {}, {}, {}, {}, lub od razu smakować najlepsze dania restauracjii #{nazwa_restauracji}."
 		@imię = imię
+		@nazwa_restauracji = nazwa_restauracji
 		case typ_jedzenia
 		when :Wegan
 			sleep 1
@@ -50,13 +53,36 @@ class Zjedzmy
 		puts "#{@@Wszystkie_stoły}"
 	end
 	def zjedzmy(proponuję)
-		puts "do stołu #{@@stół_Roślinożerców} mam trawę+#{['herbatę Roibos', 'oranżadę z jagód', 'lemoniadę z truskawek', 'sok z borówek'].sample}.
+		puts "dla stołu #{@@stół_Roślinożerców} mam trawę+#{['herbatę Roibos', 'oranżadę z jagód', 'lemoniadę z truskawek', 'sok z borówek'].sample}.
 		do stołu #{@@stół_PCz} mam #{["#{proponuję}",'kurczak i surówka+herbata Matcha gratis!', 'purre z ziemniaków i marchewki z dorszem i kawą.'].sample}
-		do stołu #{@@stół_Wegan} mam #{["#{proponuję}", "#{proponuję} i lemoniadę", "#{proponuję} i #{["napój z róży", "jogurt z mleka migdałowego"].sample}"]}!
-		do stółu #{@@stół_Wegetarian} mam #{proponuję}!"
+		dla stołu #{@@stół_mięsożerców} mam #{[:sarnę, :borsuka, :niedźwiedzia].sample}!
+		do stołu #{@@stół_Wegan} mam #{["#{proponuję}", "#{proponuję} i lemoniadę", "#{proponuję}].sample} i #{["napój z róży", "jogurt z mleka migdałowego"].sample}"]}!
+		dla stołu #{@@stół_wszystkożerców} mam #{["'pyszną' padlinkę łosia", "trawę+padlinę+herbatkę malinową"].sample} i pyszny dodatek: #{[:bezkręgowiec, "Ślimak", "gąbka prosto z morza!!"].sample}!
+		do stołu #{@@Wszystkie_stoły} mam #{proponuję}.
+		dla stółu #{@@stół_Wegetarian} mam #{["#{proponuję}", "#{proponuję}, ryż z cukinią i sałatkę owocową"].sample}+#{["sok z truskawki.", :sok, "kawa!", "Matcha!"].sample}"
 	end
 	def dodaj_do_stołów(imię)
-		print "czy #{imię} je wszystko?"
-    #tu powinno być więcej
+		while true
+			print "czy #{imię} je wszystko(jako zwierzę, nie człowiek)?"
+			tak_lub_nie = gets.chomp
+			if tak_lub_nie == 'tak'
+				Wszystkożerca::nie_jem.each do |nie_jem|
+					puts "#{imię} mówi 'nie jem #{nie_jem}'."
+					sleep 1
+				end
+				@@stół_wszystkożerców = @@stół_wszystkożerców.merge ({"#{imię}" => Time.now})
+				break
+			else
+				puts "#{imię} nie mówi 'nie jem #{nie_jem}'."
+				sleep 2
+			end
+			print "czy #{imię} je jako człowiek wszystko, co się da?"
+			tlubn = gets.chomp
+			if tlubn == 'tak'
+				PopularnyCzłowiek::nie_jem.each do |nie_jem|
+					puts "'nie jem #{nie_jem}' - mówi #{imię}."
+				end
+			end
+		end
 	end
 end
