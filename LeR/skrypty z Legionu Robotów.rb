7d2f6b1424f499c85fe7a0cb2adfb7fe89cb0f8a
Stefcia <nowar3@wp.pl>
2017-04-22 15:27:23 -0400

    LeRskrypt.rb

---
+++ LeR/skrypty z Legionu Robotów.rb
@@ -0,0 +1,10 @@
+#require 'C:\Users\admin\Documents\ruby\LeR\Robowalka.rb'
+r1 = Robot.new(:r4, 55, 10,10, 50, 25)
+r2 = Robot.new("r#{[:s. "t", 5].object_id}", 77, 5,5, 40, 45)
+super_r = Robot.new("Eerno1",67, 19, 10, 15, 70)
+bubu_robo = Robot.new("Eerno2",67, 6, 5, 50, 50)
+dr = RoboPtak.new(3,4,"Gge67")
+robo = Robot.new("Zżylla5", 79, 23, 9, 55, 60)
+średni_r = Robot.new("Robo średniak.R#{rand(100)}", 50, 10, 7, 35, 50)
+w1 = RoboWalka.new(robo, super_r)
+w1.walczmy
