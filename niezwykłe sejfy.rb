85e270ddc476cea53498a71a047affc3a1e4ef9b
Stefcia <nowar3@wp.pl>
2017-05-17 15:44:59 +0200

    Update niezwykłe sejfy.rb

--- niezwykłe sejfy.rb
+++ niezwykłe sejfy.rb
@@ -1,9 +1,9 @@
 class Sejfy
 	def initialize(do_utworzenia)
 		@zrobione_sejfy_prezentowe = []
-		(-100..do_utworzenia).each do |numerek|
+		(0..do_utworzenia).each do |numerek|
 			nazwa_prezentu = "sejf_#{numerek}.txt"
-			zrobione_sejfy_prezentowe << nazwa_prezentu
+			@zrobione_sejfy_prezentowe << nazwa_prezentu
 			file = File.open(nazwa_prezentu, 'w+')
 			file.write("niezwykły sejf załadunkowy nr #{numerek} o nazwie #{nazwa_prezentu} rozdaje #{[4,6,7,9,45,10000,34,1,2,3,5,8,10,100,1000,600].sample} prezentów!")
 			file.close
@@ -32,7 +32,7 @@ class Sejfy
 			if jaki_sejf.include? (sejf)
 				file = File.open(sejf, "w")
 				file.write("sejf #{sejf} zmienia swoją treść na #{co} i jest z dodatkowym prezentem:
-				#{["piękny domek Belli!", "zamek Gisneya!!","domek z piernika i cytrynowego lukru."].sample}")
+				#{["piękny domek Belli!", "zamek Disneya!!","domek z piernika i cytrynowego lukru."].sample}")
 				file.close
 			else
 				puts "nie chcesz w #{sejf} mieć #{co}?
