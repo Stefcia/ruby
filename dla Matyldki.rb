9a7299ad25b24f938a1ab3649a3cf35fc6a70c8e
Stefcia <nowar3@wp.pl>
2017-05-26 16:46:36 +0200

    Create dla Matyldki.rb

---
+++ dla Matyldki.rb
@@ -0,0 +1,20 @@
+print "wpisz swój login:"
+zabawy = [:Kolki_Babolki, :restauracje_split, :zgadywanki, :rozmówki]
+zzabawy = [:zgadywanki, :restauracje_split, :rozmówki]
+zzabawy2 = [:Kolki_Babolki, :zgadywanki]
+kontoM = {
+	:hasło => "Matyldka",
+	:zabawy => zabawy,
+	:nazwa_użytkownika => "Matyldka KB",
+	:login => 2345
+}
+kontoS = {
+	:hasło => "RC programistka",
+	:zabawy => zzabawy,
+	:nazwa_użytkownika => "Stefka PRC"
+}
+kontoW = {
+	:hasło => "Wiktuś",
+	:zabawy => zzabawy2,
+	:nazwa_użytkownika => "Wiktuś KB"
+}
