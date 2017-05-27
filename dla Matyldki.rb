2185aa303e78bb18db6e5e87622c5acf9f6d2488
Stefcia <nowar3@wp.pl>
2017-05-27 08:16:09 +0200

    Update dla Matyldki.rb

--- dla Matyldki.rb
+++ dla Matyldki.rb
@@ -1,20 +1,29 @@
 print "wpisz swój login:"
+login = gets.chomp
 zabawy = [:Kolki_Babolki, :restauracje_split, :zgadywanki, :rozmówki]
 zzabawy = [:zgadywanki, :restauracje_split, :rozmówki]
 zzabawy2 = [:Kolki_Babolki, :zgadywanki]
 kontoM = {
+	:login => 12345, 
 	:hasło => "Matyldka",
 	:zabawy => zabawy,
-	:nazwa_użytkownika => "Matyldka KB",
-	:login => 2345
+	:grupa => "KB"
+	:nazwa_użytkownika => "Matyldka"
 }
 kontoS = {
-	:hasło => "RC programistka",
+	:login => 5682,
+	:hasło => "Stefuszka PRC",
 	:zabawy => zzabawy,
-	:nazwa_użytkownika => "Stefka PRC"
+	:grupa => "PRC",
+	:nazwa_użytkownika => "Stefka"
 }
 kontoW = {
+	:login => 4000,
 	:hasło => "Wiktuś",
 	:zabawy => zzabawy2,
-	:nazwa_użytkownika => "Wiktuś KB"
+	:grupa => "KB",
+	:nazwa_użytkownika => "Wiktuś"
 }
+kontoW.each do |nazwa, dane|
+
+end
