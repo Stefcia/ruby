a5d8d8ce892a8a603a2ab4d38da9b072cb096bef
Stefcia <nowar3@wp.pl>
2017-05-28 07:51:28 -0400

    Update dla Matyldki.rb

--- dla Matyldki.rb
+++ dla Matyldki.rb
@@ -1,8 +1,32 @@
-print "wpisz swój login(czyli imię):"
+print "wpisz swój login:"
 imię = gets.chomp
-zabawy = [:Kolki_Babolki, :restauracje_split, :zgadywanki, :rozmówki]
-zzabawy = [:zgadywanki, :restauracje_split, :rozmówki]
+sleep 3
+system ('cls')
+zabawy = [:Kolki_Babolki, :restauracje_split,:notesik, :zgadywanki, :rozmówki]
+zzabawy = [:zgadywanki, :restauracje_split, :rozmówki, :notesik]
 zzabawy2 = [:Kolki_Babolki, :zgadywanki]
+sleep 5
+def notesik(ilośćZakręceń)
+	puts "zacznij 'piękne' notatki:"
+	i = 0
+	notatki = ["oto piękna notatka:"]
+	while true
+		sleep 3
+		n = gets.chomp
+		notatki = notatki.push "#{n}"
+		sleep 1
+		if i == ilośćZakręceń
+			break
+		else
+			i = i+1
+			puts "jeszcze nie kończymy, kontynuuj:"
+		end
+	end
+	puts "oto twoje notatki:"
+	notatki.each do |n2|
+		puts n2
+	end
+end
 sleep 1
 kontoM = {
 	:hasło => "Matyldka",
@@ -10,12 +34,14 @@ kontoM = {
 	:grupa => "KB",
 	:nazwa_użytkownika => "Matyldka"
 }
+sleep rand(4)
 kontoS = {
 	:hasło => "Stefuszka PRC",
 	:zabawy => zzabawy,
 	:grupa => "PRC",
 	:nazwa_użytkownika => "Stefka"
 }
+sleep 3
 kontoW = {
 	:hasło => "Wiktuś",
 	:zabawy => zzabawy2,
@@ -25,6 +51,8 @@ kontoW = {
 daneUżytkownikow = [kontoS, kontoM, kontoW]
 puts "podaj hasło:"
 hasło = gets.chomp
+sleep 1
+system ('cls')
 #def sprawdź = rozwiązanie
 def sprawdź(podaneImie, podaneHaslo, coSprawdzam)
 	coSprawdzam.each do |a|
@@ -36,7 +64,15 @@ def sprawdź(podaneImie, podaneHaslo, coSprawdzam)
 		end
 	end
 end
+sleep 2
 tlf = sprawdź imię, hasło, daneUżytkownikow
+sleep 1
+puts "sprawdzone, czy masz poprawne dane."
 if tlf == true
+	tajne = :tak
+else
+	tajne = :nie
+end
+if tajne == :nie
 	
 end
