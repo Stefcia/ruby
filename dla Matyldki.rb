27cba34d3a364954178c192081def76771087237
Stefcia <nowar3@wp.pl>
2017-05-28 06:18:51 -0400

    nowe logowanie.rb

--- dla Matyldki.rb
+++ dla Matyldki.rb
@@ -1,36 +1,42 @@
-print "wpisz swój login:"
-login = gets.chomp
+print "wpisz swój login(czyli imię):"
+imię = gets.chomp
 zabawy = [:Kolki_Babolki, :restauracje_split, :zgadywanki, :rozmówki]
 zzabawy = [:zgadywanki, :restauracje_split, :rozmówki]
 zzabawy2 = [:Kolki_Babolki, :zgadywanki]
+sleep 1
 kontoM = {
-	:login => 12345, 
 	:hasło => "Matyldka",
 	:zabawy => zabawy,
-	:grupa => "KB"
+	:grupa => "KB",
 	:nazwa_użytkownika => "Matyldka"
 }
 kontoS = {
-	:login => 5682,
 	:hasło => "Stefuszka PRC",
 	:zabawy => zzabawy,
 	:grupa => "PRC",
 	:nazwa_użytkownika => "Stefka"
 }
 kontoW = {
-	:login => 4000,
 	:hasło => "Wiktuś",
 	:zabawy => zzabawy2,
 	:grupa => "KB",
 	:nazwa_użytkownika => "Wiktuś"
 }
-
+daneUżytkownikow = [kontoS, kontoM, kontoW]
+puts "podaj hasło:"
+hasło = gets.chomp
 #def sprawdź = rozwiązanie
-def sprawdź
-	
-end
-kontoW.each do |nazwa, dane|
-	if nazwa == dane
-		
+def sprawdź(podaneImie, podaneHaslo, coSprawdzam)
+	coSprawdzam.each do |a|
+		case a.class
+		when Hash
+			if a[:nazwa_użytkownika] == podaneImie
+				return a[:hasło] == podaneHaslo
+			end
+		end
 	end
+end
+tlf = sprawdź imię, hasło, daneUżytkownikow
+if tlf == true
+	
 end
