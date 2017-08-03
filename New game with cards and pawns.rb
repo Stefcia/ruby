78425e13213ec3203b7c2a448f99609b7fd0cfef
Stefcia <nowar3@wp.pl>
2017-08-03 16:09:53 +0200

    New game with card and pawns.rb

--- New game with cards and pawns.rb
+++ New game with cards and pawns.rb
@@ -5,25 +5,35 @@ def instrukcja
 	3.) Komputer tworzy 'planszę'.
 	4.) Wpisujesz cyfrę.
 	5.) Komputer losuje parę razy.
-	6.) liczba wylosowana przez komputer jest tą, która"
+	6.) Załóżmy, że ostateczne losowanie komputera wynosi 3. Idziesz wtedy 3 metry przed siebie.
+	7.) Jeśli w kroku przy kroku 5 trzy metry przed tobą nie ma żadnej pozycjii karty Akcjii ani pola wymagającego karty Akcjii, to nic się nie dzieje.
+	8.) Ale jeżeli jednak jest, to się dzieje... A! Sam zobacz!
+	9.) Kroki: 5, 6, 7, 8 komputer powtarza wiele razy, aż dotrzesz do końca.
+	10.) "
 end
+
 def menu_glowne
 	sleep 4.3
 	puts "Wpisz, co mam wykonać."
 	sleep 3
-	print "To znaczy... Wybierz i wpisz wybraną rzecz(którą wykonam): instrukcja, graj, wyjdz(nie można używać ż, ś, ę, ć, ó, ą, ź, więc 'wyjdz' może oznaczać 'wyjdź'. itp.):"
+	print "To znaczy... Wybierz i wpisz wybraną rzecz(którą wykonam): slownik, instrukcja, graj, wyjdz(nie można używać ż, ś, ę, ć, ó, ą, ź, więc 'wyjdz' może oznaczać 'wyjdź'. itp.):"
 	wybor = gets.chomp
 	sleep 2
 	puts "#{wybor}... wyszukuję... proszę czekać... #{wybor}..."
 	sleep 1
 	puts "#{wybor}..."
-	case wybor
-	when "wyjdz"
-		system ('cls')
-		break
-	when "instrukcja"
-		instrukcja
-	end
+end
+#po menu_glowne jest:
+#case wybor
+#when "wyjdz"
+#	system ('cls')
+#	break
+#when "instrukcja"
+#	instrukcja
+#when "graj"
+#end
+def graj (wykonaj)
+
 end
 sleep 1
 puts "Właśnie stworzono menu główne gry 'niezwykła podróż'"
